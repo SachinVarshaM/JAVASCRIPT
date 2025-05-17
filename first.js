df3162693a3db013ef8c1c921c8155efd01b2dac
unknown <sachin.mh1001@gmail.com>
2025-05-17 23:18:53 +0530

    JAVASCRIPT

---
+++ first.js
@@ -0,0 +1,16 @@
+name="Sachin Mane";
+let age=31+" Years";
+price=99.11;
+x=null;
+$y=undefined;
+isFlag=true;
+console.log('My name:',name);
+console.log('My age:',age);
+console.log('Price:',price);
+
+console.log($y);
+
+let fname="Sachin",lname="Mane",myage=31;
+
+console.log(fname," ",lname," ",myage," ",isFlag);
+
